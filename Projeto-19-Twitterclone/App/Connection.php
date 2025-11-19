efdd4fabdb3308051bc4826642e885c06b9f1103
jaype10 <42478433+jaype10@users.noreply.github.com>
2025-11-19 10:27:15 -0300

    Projeto 19 - Inicio

---
+++ Projeto-19-Twitterclone/App/Connection.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+class Connection
+{
+    public static function getDB()
+    {
+        try {
+            $conn = new \PDO(
+                "mysql:host=localhost;dbname=twitter_clone;charset=utf8",
+                "root",
+                ""
+            );
+
+            return $conn;
+        } catch (\PDOException $e) {
+            # .. tratar de alguma forma
+        }
+    }
+}
